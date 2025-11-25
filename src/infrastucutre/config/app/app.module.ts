ea2d5abcdf6bc4453199fd5b6a251bb75828f403
lanstheprodigy <alan.040219.gam@gmail.com>
2025-11-25 18:13:46 +0700

    fix: remove unused cache manager

--- src/infrastucutre/config/app/app.module.ts
+++ src/infrastucutre/config/app/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnimeModule } from 'src/core/anime/anime.module';
 import { ConfigModule } from '@nestjs/config';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development', '.env'],
-    }),
-    CacheModule.register({
-      ttl: 60 * 60 * 1000, // 1 hour
     }),
     AnimeModule,
   ],
